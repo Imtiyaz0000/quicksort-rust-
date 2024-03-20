14dff01672f8bfff063931faef7c51447be8e74b
Imtiyaz0000 <159421205+Imtiyaz0000@users.noreply.github.com>
2024-03-20 14:39:42 +0000

    gtg

--- quicksort/src/main.rs
+++ quicksort/src/main.rs
@@ -1,16 +1,17 @@
-use std::mem::{self, swap};
+use std::mem;
 
 fn quicksort(arr: &mut Vec<i64>) -> &mut Vec<i64> {
     if arr.len() <= 1 {
         return arr;
     }
     let mut p1: usize = 0;
-    let p2: usize = arr.len() - 1;
+    let mut p2: usize = arr.len() - 1;
     while p1 != p2 {
         if (arr[p1] > arr[p2] && p1 > p2) || (arr[p1] < arr[p2] && p1 < p2) {
             let mut temp = arr.clone();
-            swap(&mut arr[p1], &mut temp[p2]);
-            swap(&mut arr[p2], &mut temp[p1]);
+            mem::swap(&mut arr[p1], &mut temp[p2]);
+            mem::swap(&mut arr[p2], &mut temp[p1]);
+            mem::swap(&mut p1,&mut p2);
 
             if p1 > p2 {
                 p1 -= 1;
@@ -19,7 +20,10 @@ fn quicksort(arr: &mut Vec<i64>) -> &mut Vec<i64> {
             }
         }
     }
-    arr
+    let mut left = quicksort(&mut arr[..p1].to_vec());
+    let mut right = quicksort(&mut arr[p1+1..].to_vec());
+
+    left.to
 }
 
 fn main() {
